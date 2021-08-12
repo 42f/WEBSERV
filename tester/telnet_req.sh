729df1bdb1c2df6318b102f19d9732bdf0b06840
bvalette <bvalette@student.42.fr>
2021-08-12 21:43:37 +0200

    [ Thu-12.08-21:43 ] [DEV] add tester tools

---
+++ tester/telnet_req.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+request() {
+	echo "open $1 $2"
+	sleep 2
+	echo "GET $4 HTTP/1.0"
+	echo "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.4) Gecko/20070515 Firefox/2.0.0.4"
+	echo "Host: $3"
+	echo
+	echo
+	sleep 2
+}
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    echo "./telnet_req.sh [IP] [PORT] [HOST_HEADER] [TARGET]"
+    echo "ex.: ./telnet_req.sh [127.0.0.1] [18000] [localhost] [/]"
+    exit 1
+else
+	request $1 $2 $3 $4 | telnet
+fi
+
