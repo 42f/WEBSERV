3231a7749d13122ac88bce4be74918afa6067fde
Brian Valette <bvalette@student.42.fr>
2021-08-18 12:12:06 +0200

    [ Wed-18.08-12:12 ] [DEV] push

--- assets/HTML_for_locationtest/server/0/delete.js
+++ assets/HTML_for_locationtest/server/0/delete.js
@@ -2,6 +2,8 @@
 
 function sendDelete () {
   var oReq = new XMLHttpRequest();
-  oReq.open("DELETE", "/site0/anotherfile.html");
+  oReq.open("DELETE", "http://localhost:18000/site0/anotherfile.html");
+
+
   oReq.send();
 }
