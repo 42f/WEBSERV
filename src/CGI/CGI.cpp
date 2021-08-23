2fec5db5ceaeb240f1be742b6f21afc88aa18fe4
Ovoda <cdetourrispro@gmail.com>
2021-08-23 14:32:06 +0200

    [ Mon-23.08-14:32 ] [DEV] cgi binary checlk

--- src/CGI/CGI.cpp
+++ src/CGI/CGI.cpp
@@ -14,6 +14,7 @@ cgi_status::status CGI::status(void) {
   if (ret == _child_pid) {
     // std::cout << "status is done" << std::endl;
     if (_child_return < 0) {
+      std::cout << "execve returned : " << _child_return << std::endl;
       _status = cgi_status::CGI_ERROR;
     } else {
       _status = cgi_status::DONE;
@@ -41,50 +42,27 @@ std::vector<char *> CGI::set_meta_variables(std::string cgi_path,
 
   struct stat sb;
 
-  // TODO : finish error management here
-  if (stat(cgi_path.c_str(), &sb) == -1) {
-    perror("stat cgi path");
-    _status = cgi_status::SYSTEM_ERROR;
-  }
-
-  //----------------------------------------
   add_variable("AUTH_TYPE", "");
-  //----------------------------------------
   add_variable("REMOTE_USER", "");
-  //----------------------------------------
   add_variable("REMOTE_HOST", "");
-  //----------------------------------------
   add_variable("REMOTE_IDENT", "");
-  //----------------------------------------
-  add_variable("QUERY_STRING", "");
-  //----------------------------------------
+  if (req.target.query.empty()) {
+    add_variable("QUERY_STRING", "");
+  } else {
+    add_variable("QUERY_STRING", req.target.query);
+  }
   add_variable("SERVER_SOFTWARE", "");
-  //----------------------------------------
   add_variable("REDIRECT_STATUS", "200");
-  //----------------------------------------
   add_variable("PATH_INFO", file.getPath());
-  //----------------------------------------
-  add_variable("SCRIPT_NAME",
-               file.getFileFromPath(
-                   file.getPath()));  // end of filename, ex : /index.php
-  //----------------------------------------
+  add_variable("SCRIPT_NAME", file.getFileFromPath(file.getPath()));
   add_variable("SERVER_PROTOCOL", "HTTP/1.1");
-  //----------------------------------------
   add_variable("GATEWAY_INTERFACE", "CGI/1.1");
-  //----------------------------------------
   add_variable("SERVER_PORT", serv.get_port());
-  //----------------------------------------
   add_variable("SERVER_NAME", serv.get_name());
-  //----------------------------------------
   add_variable("REMOTE_ADDR", req.get_client_ip());
-  //----------------------------------------
   // variables.push_back("SCRIPT_NAME=cgi_info.php");
-  //----------------------------------------
   add_variable("PATH_TRANSLATED", file.getPath());
-  //----------------------------------------
-  add_variable("SCRIPT_FILENAME",
-               file.getPath());  // path absolu ? tmp/server/etc...
-  //----------------------------------------
+  add_variable("SCRIPT_FILENAME", file.getPath());
   if (req.method == methods::GET)
     add_variable("REQUEST_METHOD", "GET");
   else if (req.method == methods::POST)
@@ -95,21 +73,18 @@ std::vector<char *> CGI::set_meta_variables(std::string cgi_path,
     _status = cgi_status::UNSUPPORTED;
     return variables;
   }
-  //----------------------------------------
   Result<std::__1::string> content_length = req.get_header("content-length");
   if (content_length.is_ok()) {
     add_variable("CONTENT_LENGTH", content_length.unwrap());
   } else {
     add_variable("CONTENT_LENGTH", "");
   }
-  //----------------------------------------
   Result<std::__1::string> content_type = req.get_header("content-type");
   if (content_type.is_ok()) {
     add_variable("CONTENT_TYPE", content_type.unwrap());
   } else {
     add_variable("CONTENT_TYPE", "");
   }
-  //----------------------------------------
 
   return variables;
 }
@@ -131,26 +106,35 @@ void CGI::execute_cgi(std::string cgi_path, files::File const &file,
   env[i] = NULL;
 
   char *cgi = strdup(cgi_path.c_str());
+  if (cgi == NULL) {
+    _status = cgi_status::SYSTEM_ERROR;
+    return;
+  }
   char *args[] = {cgi, NULL};
 
   if (pipe(pipes) < 0) {
-    perror("pipes");
+    perror("System Error : pipe()");
     _status = cgi_status::SYSTEM_ERROR;
     return;
   }
 
   _child_pid = fork();
   if (_child_pid < 0) {
-    perror("fork");
+    perror("System Error : fork()");
     _status = cgi_status::SYSTEM_ERROR;
     return;
   }
   if (_child_pid == 0) {
     close(pipes[0]);
-    dup2(pipes[1], 1);
+    if (dup2(pipes[1], 1) < 0) {
+      perror("System Error : dup2()");
+      _status = cgi_status::SYSTEM_ERROR;
+      return;
+    }
     close(pipes[1]);
-    execve(args[0], args, env);
-    exit(1);
+    if (execve(args[0], args, env) < 0) {
+      exit(1);
+    }
   } else {
     waitpid(_child_pid, &_child_return, WNOHANG);
     close(pipes[1]);
--- src/Config/Directives/Cgi.cpp
+++ src/Config/Directives/Cgi.cpp
@@ -8,13 +8,23 @@
  * Cgi = cgi .extension Path
  */
 
-Cgi::Cgi() { }
+Cgi::Cgi() {}
 
-Cgi::result_type Cgi::operator()(const slice &input)
-{
-	return sequence(delimited(sequence(Tag("cgi"), rws),
-				preceded(Tag("."), TakeUntil(" ")),
-			rws),TakeUntil(";"))(input);
+Cgi::result_type Cgi::operator()(const slice &input) {
+  result_type result =
+      sequence(delimited(sequence(Tag("cgi"), rws),
+                         preceded(Tag("."), TakeUntil(" ")), rws),
+               TakeUntil(";"))(input);
+
+  if (result.is_ok()) {
+    std::string cgi_path = result.unwrap().second.to_string();
+    if (access(cgi_path.c_str(), X_OK))
+    {
+      return result_type::fail(
+          result.left(), error("CGI path does not exist or is non executable by the current user"));
+    }
+  }
+  return result;
 }
 
 /* ************************************************************************** */
