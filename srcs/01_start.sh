ba408b17c48e5b28d94bd6a4bb5b6f96b35f110c
bvalette <bvalette@student.42.fr>
2021-09-09 15:35:08 +0200

    [ Thu-09.09-15:35 ] [DEV] add volume to be mounted in the container in order to have nginx serve host files to run easy tests

--- srcs/01_start.sh
+++ srcs/01_start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-
-docker run -it -p 80:80 -p 8080:8080 -p443:443 bv/ft_server
+VOLUME=$(ls -d $PWD/volume)
+docker run --rm -it -p 80:80 -p 8080:8080 -p443:443 -i --mount type=bind,src=$VOLUME,dst=/var/www/volume bv/ft_server
---
+++ volume/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// throw new Exception("Division by zero", 1);
+
+	echo "Hello from PHP";
+	header("Hello: from PHP");
+	sleep(120);
+?>
